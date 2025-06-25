ff338f38fdbb8da38860e0e29f4e7410b3e3054a
Jurandi Junior <jurandijr20@gmail.com>
2025-06-24 22:52:16 -0300

    Demo 1

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
-﻿internal class Program
+﻿using System;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        
+        DateTime dt = new DateTime(2025, 06, 24, 8, 10, 45);
+        Console.WriteLine(dt.ElapsedTime());
     }
 }
