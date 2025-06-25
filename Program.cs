302c9dbdf4cbcd184ce1fb16d3eeed52e60a9eed
Jurandi Junior <jurandijr20@gmail.com>
2025-06-24 22:58:38 -0300

    Demo 2

--- Program.cs
+++ Program.cs
@@ -6,5 +6,8 @@ internal class Program
     {
         DateTime dt = new DateTime(2025, 06, 24, 8, 10, 45);
         Console.WriteLine(dt.ElapsedTime());
+
+        String s1 = "Good morning dear students!";
+        Console.WriteLine(s1.Cut(10));
     }
 }
